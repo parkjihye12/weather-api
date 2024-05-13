055e8062768bacf19dbacec2c16da67585433d06
parkjihye12 <qkr-wlg@hanmail.net>
2024-05-13 16:19:37 +0900

    enter

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,8 +1,22 @@
+import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+
 const WeatherIcon = () => {
   const [latitude, setLatitude] = useState();
   const [longitude, setLongitude] = useState();
+
+  const getWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=8014ca43663f34c6e3a9bb8a0d41082c&units=metric`
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -10,6 +24,12 @@ const WeatherIcon = () => {
       setLongitude(position.coords.longitude);
     });
   }, []);
+
+  useEffect(() => {
+    if (!latitude) return;
+
+    getWeather();
+  }, [latitude]);
 
   return <div>WeatherIcon</div>;
 };
